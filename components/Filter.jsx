041c384d01154192b8de0c7c2cdbc6d403082d3b
Jess <jessica.wilson@jisc.ac.uk>
2023-08-29 20:46:08 +0100

    this would be the filter for the table

---
+++ components/Filter.jsx
@@ -0,0 +1,12 @@
+const Filter = () => {
+	return (
+		<div>
+			<select>
+				<option value='someOption'>Some option</option>
+				<option value='otherOption'>Other option</option>
+			</select>
+		</div>
+	);
+};
+
+export default Filter;
